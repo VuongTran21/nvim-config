b906277873ad7a632d37ab9476bc8933b9bf0f50
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2022-04-18 22:55:22 +0700

    remove sumneko lua

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -16,38 +16,3 @@ require'lspconfig'.pyright.setup{
   capabilities = capabilities,
 }
 
-print('kaka')
-local sumneko_root_path = vim.fn.exepath('nvim')
-print('test', sumneko_root_path)
-local sumneko_binary = vim.fn.fnamemodify(sumneko_root_path, ':h:h:h') 
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_root_path, "-E", sumneko_binary .. "/main.lua"};
-    settings = {
-        Lua = {
-        runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-        },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-            enable = false,
-        },
-        },
-    },
-}
-
